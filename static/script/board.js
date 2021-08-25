fd04c0f71544f1ea0307ce406391efc75fccd368
uchuukaeru <hirata.motoki@gmail.com>
2021-08-25 18:04:37 +0900

    Update board.js

--- static/script/board.js
+++ static/script/board.js
@@ -10,19 +10,6 @@ const enc = (s) => {
     return s;
 }
 
-/*
-window.onload = async () => {
-    const data = await fetchJSON("api/blist",boardID);
-    for (const d of data) {
-        const div = document.createElement("div");
-        div.className = "bbsitem";
-        div.innerHTML = `<span class=date>${enc(d.date)}</span> by <span class=name>${enc(d.name)}</span>
-            <div class=body>${enc(d.body)}</div>`
-        container.appendChild(div);
-    }
-}
-*/
-
 async function add_board(boardID){
     console.log("call add-board");
     const item = {
